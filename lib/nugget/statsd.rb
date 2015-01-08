b07aa23161745ecc35d76ee19de9218c11879376
Aman Gupta <aman@tmm1.net>
2015-01-07 22:17:54 -0800

    Revert "resolve statsd ip upfront"
    This reverts commit 9773b06e435332e367933bb65f016183e6ad4020.

--- lib/nugget/statsd.rb
+++ lib/nugget/statsd.rb
@@ -2,8 +2,7 @@ module Nugget
   class NStatsd # NStatsd since Statsd collides
 
     def self.stats
-      ip = Addrinfo.ip(Nugget::Config.statsd_host)
-      @stats ||= Statsd.new(ip, Nugget::Config.statsd_port, Nugget::Config.statsd_key).tap do |statsd|
+      @stats ||= Statsd.new(Nugget::Config.statsd_host, Nugget::Config.statsd_port, Nugget::Config.statsd_key).tap do |statsd|
         statsd.namespace =  Nugget::Config.statsd_namespace
       end
     end
